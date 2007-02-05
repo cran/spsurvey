83736e385dfd21c45bdc00a6824ddd0754020935
Tom Kincaid <Kincaid.Tom@epa.gov>
2007-02-05 00:00:00 +0000

    version 1.6.1

--- R/onLoad.r
+++ R/onLoad.r
@@ -1,3 +1,3 @@
 .onLoad <- function(lib, pkg) {
-   cat("\nVersion 1.6 of the spsurvey library was loaded successfully.\n\n")
+   cat("\nVersion 1.6.1 of the spsurvey library was loaded successfully.\n\n")
 }
