df35a1341b7b3b4e7c4228db538ffc4bd4b90a8f
Michael Dumelle <Dumelle.Michael@epa.gov>
2022-11-22 08:10:06 +0000

    version 5.4.0

--- inst/doc/EDA.R
+++ inst/doc/EDA.R
@@ -13,73 +13,76 @@ knitr::opts_chunk$set(
 library(spsurvey)
 
 ## -----------------------------------------------------------------------------
-sp_summary(NE_Lakes, formula = ~ ELEV)
-
-## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(NE_Lakes, formula = ~ ELEV)
-
-## ---- echo = FALSE------------------------------------------------------------
-sp_plot(NE_Lakes, formula = ~ ELEV, key.pos = 4)
+NE_Lakes <- sp_frame(NE_Lakes)
 
 ## -----------------------------------------------------------------------------
-sp_summary(NE_Lakes, formula = ~ ELEV_CAT)
+summary(NE_Lakes, formula = ~ ELEV)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(NE_Lakes, formula = ~ ELEV_CAT, key.width = lcm(3))
+#  plot(NE_Lakes, formula = ~ ELEV)
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(NE_Lakes, formula = ~ ELEV_CAT, key.width = lcm(3), key.pos = 4)
+plot(NE_Lakes, formula = ~ ELEV, key.pos = 4)
 
 ## -----------------------------------------------------------------------------
-sp_summary(NE_Lakes, formula = ~ ELEV_CAT + AREA_CAT)
+summary(NE_Lakes, formula = ~ ELEV_CAT)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(NE_Lakes, formula = ~ ELEV_CAT + AREA_CAT, key.width = lcm(3))
+#  plot(NE_Lakes, formula = ~ ELEV_CAT, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(NE_Lakes, formula = ~ ELEV_CAT + AREA_CAT, key.width = lcm(3), key.pos = 4)
+plot(NE_Lakes, formula = ~ ELEV_CAT, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
-sp_summary(NE_Lakes, formula = ~ ELEV_CAT:AREA_CAT)
+summary(NE_Lakes, formula = ~ ELEV_CAT + AREA_CAT)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(NE_Lakes, formula = ~ ELEV_CAT:AREA_CAT, key.width = lcm(3))
+#  plot(NE_Lakes, formula = ~ ELEV_CAT + AREA_CAT, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(NE_Lakes, formula = ~ ELEV_CAT:AREA_CAT, key.width = lcm(3), key.pos = 4)
+plot(NE_Lakes, formula = ~ ELEV_CAT + AREA_CAT, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
-sp_summary(NE_Lakes, formula = ELEV ~ AREA_CAT)
+summary(NE_Lakes, formula = ~ ELEV_CAT:AREA_CAT)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(NE_Lakes, formula = ELEV ~ AREA_CAT)
+#  plot(NE_Lakes, formula = ~ ELEV_CAT:AREA_CAT, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(NE_Lakes, formula = ELEV ~ AREA_CAT, key.pos = 4)
+plot(NE_Lakes, formula = ~ ELEV_CAT:AREA_CAT, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
-sp_summary(NE_Lakes, formula = ELEV ~ AREA_CAT, onlyshow = "small")
+summary(NE_Lakes, formula = ELEV ~ AREA_CAT)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(NE_Lakes, formula = ELEV ~ AREA_CAT, onlyshow = "small")
+#  plot(NE_Lakes, formula = ELEV ~ AREA_CAT)
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(NE_Lakes, formula = ELEV ~ AREA_CAT, onlyshow = "small", key.pos = 4)
+plot(NE_Lakes, formula = ELEV ~ AREA_CAT, key.pos = 4)
 
 ## -----------------------------------------------------------------------------
-sp_summary(NE_Lakes, formula = ELEV_CAT ~ AREA_CAT)
+summary(NE_Lakes, formula = ELEV ~ AREA_CAT, onlyshow = "small")
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(NE_Lakes, formula = ELEV_CAT ~ AREA_CAT, key.width = lcm(3))
+#  plot(NE_Lakes, formula = ELEV ~ AREA_CAT, onlyshow = "small")
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(NE_Lakes, formula = ELEV_CAT ~ AREA_CAT, key.width = lcm(3), key.pos = 4)
+plot(NE_Lakes, formula = ELEV ~ AREA_CAT, onlyshow = "small", key.pos = 4)
+
+## -----------------------------------------------------------------------------
+summary(NE_Lakes, formula = ELEV_CAT ~ AREA_CAT)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  plot(NE_Lakes, formula = ELEV_CAT ~ AREA_CAT, key.width = lcm(3))
+
+## ---- echo = FALSE------------------------------------------------------------
+plot(NE_Lakes, formula = ELEV_CAT ~ AREA_CAT, key.width = lcm(3), key.pos = 4)
 
 ## ---- eval = FALSE------------------------------------------------------------
 #  list1 <- list(main = "Elevation Categories", pal = rainbow)
 #  list2 <- list(main = "Area Categories")
 #  list3 <- list(levels = c("small", "large"), pch = c(4, 19))
-#  sp_plot(
+#  plot(
 #    NE_Lakes,
 #    formula = ~ ELEV_CAT + AREA_CAT,
 #    var_args = list(ELEV_CAT = list1, AREA_CAT = list2),
@@ -92,7 +95,7 @@ sp_plot(NE_Lakes, formula = ELEV_CAT ~ AREA_CAT, key.width = lcm(3), key.pos = 4
 list1 <- list(main = "Elevation Categories", pal = rainbow)
 list2 <- list(main = "Area Categories")
 list3 <- list(levels = c("small", "large"), pch = c(4, 19))
-sp_plot(
+plot(
   NE_Lakes,
   formula = ~ ELEV_CAT + AREA_CAT,
   var_args = list(ELEV_CAT = list1, AREA_CAT = list2),
@@ -104,7 +107,7 @@ sp_plot(
 
 ## ---- eval = FALSE------------------------------------------------------------
 #  sublist <- list(AREA_CAT = list3)
-#  sp_plot(
+#  plot(
 #    NE_Lakes,
 #    formula = AREA_CAT ~ ELEV_CAT,
 #    var_args = list(ELEV_CAT = sublist),
@@ -113,7 +116,7 @@ sp_plot(
 
 ## ---- echo = FALSE------------------------------------------------------------
 sublist <- list(AREA_CAT = list3)
-sp_plot(
+plot(
   NE_Lakes,
   formula = AREA_CAT ~ ELEV_CAT,
   var_args = list(ELEV_CAT = sublist),
@@ -128,53 +131,56 @@ set.seed(51)
 eqprob_rho <- grts(NE_Lakes, n_base = 50, n_over = 10)
 
 ## -----------------------------------------------------------------------------
-sp_summary(eqprob_rho)
+summary(eqprob_rho)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(eqprob_rho, key.width = lcm(3))
+#  plot(eqprob_rho, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(eqprob_rho, key.width = lcm(3), key.pos = 4)
+plot(eqprob_rho, key.width = lcm(3), key.pos = 4)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(eqprob_rho, NE_Lakes, key.width = lcm(3))
+#  plot(eqprob_rho, NE_Lakes, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(eqprob_rho, NE_Lakes, key.width = lcm(3), key.pos = 4)
+plot(eqprob_rho, NE_Lakes, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
-sp_summary(eqprob_rho, formula = siteuse ~ AREA_CAT)
+summary(eqprob_rho, formula = siteuse ~ AREA_CAT)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(eqprob_rho, formula = siteuse ~ AREA_CAT, key.width = lcm(3))
+#  plot(eqprob_rho, formula = siteuse ~ AREA_CAT, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(eqprob_rho, formula = siteuse ~ AREA_CAT, key.width = lcm(3), key.pos = 4)
+plot(eqprob_rho, formula = siteuse ~ AREA_CAT, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
-sp_summary(eqprob_rho, formula = ELEV ~ siteuse)
+summary(eqprob_rho, formula = ELEV ~ siteuse)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(eqprob_rho, formula = ELEV ~ siteuse)
+#  plot(eqprob_rho, formula = ELEV ~ siteuse)
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(eqprob_rho, formula = ELEV ~ siteuse, key.pos = 4)
+plot(eqprob_rho, formula = ELEV ~ siteuse, key.pos = 4)
 
 ## -----------------------------------------------------------------------------
-sp_summary(NLA_PNW, formula = ~ NITR_COND)
-
-## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(NLA_PNW, formula = ~ NITR_COND, key.width = lcm(3))
-
-## ---- echo = FALSE------------------------------------------------------------
-sp_plot(NLA_PNW, formula = ~ NITR_COND, key.width = lcm(3), key.pos = 4)
+NLA_PNW <- sp_frame(NLA_PNW)
 
 ## -----------------------------------------------------------------------------
-sp_summary(NLA_PNW, formula = NITR_COND ~ STATE)
+summary(NLA_PNW, formula = ~ NITR_COND)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(NLA_PNW, formula = NITR_COND ~ STATE, key.width = lcm(3))
+#  plot(NLA_PNW, formula = ~ NITR_COND, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(NLA_PNW, formula = NITR_COND ~ STATE, key.width = lcm(3), key.pos = 4)
+plot(NLA_PNW, formula = ~ NITR_COND, key.width = lcm(3), key.pos = 4)
+
+## -----------------------------------------------------------------------------
+summary(NLA_PNW, formula = NITR_COND ~ STATE)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  plot(NLA_PNW, formula = NITR_COND ~ STATE, key.width = lcm(3))
+
+## ---- echo = FALSE------------------------------------------------------------
+plot(NLA_PNW, formula = NITR_COND ~ STATE, key.width = lcm(3), key.pos = 4)
 
--- inst/doc/sampling.R
+++ inst/doc/sampling.R
@@ -16,11 +16,14 @@ set.seed(51)
 ## -----------------------------------------------------------------------------
 eqprob <- grts(NE_Lakes, n_base = 50)
 
+## -----------------------------------------------------------------------------
+eqprob
+
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(eqprob, NE_Lakes, key.width = lcm(3))
+#  plot(eqprob, NE_Lakes, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(eqprob, NE_Lakes, key.width = lcm(3), key.pos = 4)
+plot(eqprob, NE_Lakes, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
 caty_n <- c(small = 40, large = 10)
@@ -30,6 +33,7 @@ uneqprob <- grts(
   caty_var = "AREA_CAT",
   caty_n = caty_n
 )
+uneqprob
 
 ## -----------------------------------------------------------------------------
 propprob <- grts(
@@ -41,9 +45,10 @@ propprob <- grts(
 ## -----------------------------------------------------------------------------
 strata_n <- c(low = 25, high = 15)
 strat_eqprob <- grts(NE_Lakes, n_base = strata_n, stratum_var = "ELEV_CAT")
+strat_eqprob
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(
+#  plot(
 #    strat_eqprob,
 #    formula = siteuse ~ ELEV_CAT,
 #    NE_Lakes,
@@ -51,7 +56,7 @@ strat_eqprob <- grts(NE_Lakes, n_base = strata_n, stratum_var = "ELEV_CAT")
 #  )
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(
+plot(
   strat_eqprob,
   formula = siteuse ~ ELEV_CAT,
   NE_Lakes,
@@ -71,6 +76,7 @@ strat_uneqprob <- grts(
   caty_var = "AREA_CAT",
   caty_n = caty_n
 )
+strat_uneqprob
 
 ## -----------------------------------------------------------------------------
 strat_propprob <- grts(
@@ -82,12 +88,13 @@ strat_propprob <- grts(
 
 ## -----------------------------------------------------------------------------
 legacy <- grts(NE_Lakes, n_base = 50, legacy_sites = NE_Lakes_Legacy)
+legacy
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(legacy, key.width = lcm(3))
+#  plot(legacy, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(legacy, key.width = lcm(3), key.pos = 4)
+plot(legacy, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
 mindis <- grts(NE_Lakes, n_base = 50, mindis = 1600)
@@ -103,12 +110,13 @@ strat_mindis <- grts(
 
 ## -----------------------------------------------------------------------------
 rho_replace <- grts(NE_Lakes, n_base = 50, n_over = 25)
+rho_replace
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(rho_replace, key.width = lcm(3))
+#  plot(rho_replace, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(rho_replace, key.width = lcm(3), key.pos = 4)
+plot(rho_replace, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
 over_list <- list(low = 2, high = 5)
@@ -118,9 +126,11 @@ strat_rho_replace <- grts(
   stratum_var = "ELEV_CAT",
   n_over = over_list
 )
+strat_rho_replace
 
 ## -----------------------------------------------------------------------------
 nn_replace <- grts(NE_Lakes, n_base = 50, n_near = 1)
+nn_replace
 
 ## -----------------------------------------------------------------------------
 near_list <- list(low = 1, high = 2)
@@ -130,19 +140,22 @@ strat_nn_replace <- grts(
   stratum_var = "ELEV_CAT",
   n_near = near_list
 )
+strat_nn_replace
 
 ## -----------------------------------------------------------------------------
 eqprob_irs <- irs(NE_Lakes, n_base = 50)
+eqprob_irs
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(eqprob_irs, NE_Lakes, key.width = lcm(3))
+#  plot(eqprob_irs, NE_Lakes, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(eqprob_irs, NE_Lakes, key.width = lcm(3), key.pos = 4)
+plot(eqprob_irs, NE_Lakes, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
 strata_n <- c(low = 25, high = 15)
 strat_eqprob_irs <- irs(NE_Lakes, n_base = strata_n, stratum_var = "ELEV_CAT")
+strat_eqprob_irs
 
 ## -----------------------------------------------------------------------------
 sp_balance(eqprob$sites_base, NE_Lakes) # grts
@@ -156,10 +169,10 @@ sp_balance(strat_eqprob_irs$sites_base, NE_Lakes, stratum_var = "ELEV_CAT") # ir
 eqprob <- grts(Illinois_River, n_base = 50)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(eqprob, Illinois_River, key.width = lcm(3))
+#  plot(eqprob, Illinois_River, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(eqprob, Illinois_River, key.width = lcm(3), key.pos = 4)
+plot(eqprob, Illinois_River, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
 legacy <- grts(Illinois_River, n_base = 50, legacy_sites = Illinois_River_Legacy)
@@ -168,10 +181,10 @@ legacy <- grts(Illinois_River, n_base = 50, legacy_sites = Illinois_River_Legacy
 eqprob <- grts(Lake_Ontario, n_base = 50)
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  sp_plot(eqprob, Lake_Ontario, pch = 19, key.width = lcm(3))
+#  plot(eqprob, Lake_Ontario, pch = 19, key.width = lcm(3))
 
 ## ---- echo = FALSE------------------------------------------------------------
-sp_plot(eqprob, Lake_Ontario, pch = 19, key.width = lcm(3), key.pos = 4)
+plot(eqprob, Lake_Ontario, pch = 19, key.width = lcm(3), key.pos = 4)
 
 ## -----------------------------------------------------------------------------
 combined <- sp_rbind(rho_replace)
